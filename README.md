2e37d5187d9f7739a8b234c36cabd08426b5807b
Kevin W Monroe <kevin.monroe@canonical.com>
2020-04-16 16:47:10 +0000

    new custom-registry-ca config

--- README.md
+++ README.md
@@ -3,10 +3,30 @@
 This is a library layer for common functions that exist across the container
 runtimes.
 
+## Configuration
 
-# Tests
+### Custom Registry CA
+
+If the container runtime needs to pull images from an external registry with
+self-signed TLS certificates, you will need to configure the runtime with the
+CA used to sign those certs:
+
+```bash
+juju config containerd custom-registry-ca=$(base64 /path/to/ca.crt)
+```
+
+>Note: this is not required when following the
+[Private Docker Registry][docker-registry] documentation, as the
+`docker-registry` charm is related to the same certificate provider as the
+rest of the Charmed Kubernetes cluster.
+
+## Tests
 
 To run tests use:
-```
+
+```bash
 python -m pytest ./tests
 ```
+
+<!-- LINKS -->
+[docker-registry]: https://ubuntu.com/kubernetes/docs/docker-registry
--- reactive/container-runtime-common.py
+++ reactive/container-runtime-common.py
@@ -1,9 +1,15 @@
+from base64 import b64decode
 from subprocess import check_call
 
-from charms.reactive import when_not
-from charms.reactive import set_state
+from charms.layer import status
+from charms.reactive import (
+    data_changed,
+    set_flag,
+    when_any,
+    when_not
+)
 
-from charmhelpers.core import hookenv
+from charmhelpers.core import hookenv, host
 
 
 @when_not('cgroups.modified')
@@ -19,5 +25,22 @@ def enable_grub_cgroups():
     if cfg.get('enable-cgroups'):
         hookenv.log('Calling enable_grub_cgroups.sh and rebooting machine.')
         check_call(['scripts/enable_grub_cgroups.sh'])
-        set_state('cgroups.modified')
+        set_flag('cgroups.modified')
 
+
+@when_any('config.set.custom-registry-ca', 'config.changed.custom-registry-ca')
+def install_custom_ca():
+    """
+    Installs a configured CA cert into the system-wide location.
+    """
+    ca_cert = hookenv.config().get('custom-registry-ca')
+    if ca_cert and data_changed('custom-registry-ca', ca_cert):
+        try:
+            # decode to bytes, as that's what install_ca_cert wants
+            _ca = b64decode(ca_cert)
+        except Exception:
+            status.blocked('Invalid value for custom-registry-ca config')
+            return
+        else:
+            hookenv.log('Installing custom registry CA')
+            host.install_ca_cert(_ca, name='juju-custom-registry')
